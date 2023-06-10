748f9251a8ad9c1b81a86f841a5e5094a56d5e99
nishu kumar <kumarnishu437@gmail.com>
2023-06-10 16:41:44 +0530

    updated

--- utils/ControlMessage.ts
+++ utils/ControlMessage.ts
@@ -1,12 +1,25 @@
-import WAWebJS from "whatsapp-web.js";
+import WAWebJS, { Buttons } from "whatsapp-web.js";
 import { client } from "./ConnectWhatsapp";
 
 export const ControlMessage = async (msg: WAWebJS.Message) => {
-    console.log(msg)
     if (client) {
+        console.log(msg)
         const from = await client.getNumberId(msg.from); // get mobile number details
+        let messageContent1: Buttons = {
+            body: 'give response through buttons',
+            buttons: [{
+                buttonId: "shdwdiwd",
+                buttonText: {
+                    displayText: "yes"
+                },
+                type: 1
+            }]
+        }
+        let messageContent2="we got you"
         if (from) {
-            const sendMessageData = await client.sendMessage(from._serialized, "hi nishu got your message"); // send message
+            console.log(from._serialized)
+            await client.sendMessage(from._serialized, messageContent1); // send message
+
             console.log("message sent")
         } else {
             console.log(from, `${from} is not registered`)
