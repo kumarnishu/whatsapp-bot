5fb4950bc4f6987b496fa5276f3d1106b57b3c32
nishu kumar <kumarnishu437@gmail.com>
2023-06-22 21:48:35 +0530

    added venom bot

--- utils/ConnectWhatsapp.ts
+++ utils/ConnectWhatsapp.ts
@@ -1,64 +1,32 @@
 import { Socket } from "socket.io";
-import WAWebJS, { Client, LocalAuth } from "whatsapp-web.js";
-import { User } from "../models/User";
 import { Request } from "express";
+import venom from "venom-bot"
 import { ControlMessage } from "./ControlMessage";
 
 
-export var client: Client | undefined = undefined;
+let  client: venom.Whatsapp | undefined = undefined;
 
 export const ConectWhatsapp = async (req: Request, client_id: string, socket: Socket) => {
-    if (client) {
-        await client.destroy()
+   await venom.create(client_id).then((cl) => {
+        client = cl
+        Start(cl)
+    }).catch((Err) => console.log(Err))
+
+    function Start(client: venom.Whatsapp) {
+        client.onMessage((message) => {
+            if (message.body === 'Hi' && message.isGroupMsg === false) {
+                ControlMessage(message)
+                client
+                    .sendText(message.from, 'Welcome Venom 🕷')
+                    .then((result) => {
+                        console.log('Result: ', result); //return object success
+                    })
+                    .catch((erro) => {
+                        console.error('Error when sending: ', erro); //return object error
+                    });
+            }
+        });
     }
-    console.log("getting session")
-    client = new Client({
-        authStrategy: new LocalAuth({
-            clientId: client_id
-        }),
-        puppeteer: {
-            headless: true,
-            args: [
-                '--no-sandbox',
-                '--disable-setuid-sandbox'
-            ]
-        }
-    });
-
-    client.on("ready", async () => {
-        socket.emit("ready", client_id)
-        await User.findByIdAndUpdate(req.user?._id, {
-            whatsapp: { client_id: client_id, is_active: true }
-        })
-        console.log("session revived", req.user?._id)
-
-    })
-    client.on("authenticated", async () => {
-        console.log("authenticated")
-        await User.findByIdAndUpdate(req.user?._id, {
-            whatsapp: { client_id: client_id, is_active: true }
-        })
-    })
-    client.on("auth_failure", async () => {
-        console.log("failed to authenticate")
-        await User.findByIdAndUpdate(req.user?._id, {
-            whatsapp: { client_id: client_id, is_active: false }
-        })
-    })
-    client.on('qr', async (qr) => {
-        console.log("logged out", qr)
-        socket.emit("qr", qr);
-        await User.findByIdAndUpdate(req.user?._id, {
-            whatsapp: { client_id: client_id, is_active: false }
-        })
-
-    });
-    client.on('loading_screen', async (qr) => {
-        console.log("loading..")
-        socket.emit("loading");
-    });
-    client.on('message', async (msg: WAWebJS.Message) => {
-        ControlMessage(msg)
-    });
-    await client.initialize();
 }
+
+export {client}
--- utils/ControlMessage.ts
+++ utils/ControlMessage.ts
@@ -1,25 +1,30 @@
-import WAWebJS, { List, MessageMedia } from "whatsapp-web.js";
+import { Message } from "venom-bot";
 import { client } from "./ConnectWhatsapp";
 
-export const ControlMessage = async (msg: WAWebJS.Message) => {
-    console.log("recieved message:", msg)
+export const ControlMessage = async (message: Message) => {
     try {
         if (client) {
-            const from = await client.getNumberId(msg.from); // get mobile number details
-            let message: WAWebJS.MessageContent | undefined;
-            // message="hello i am coming"
-            // message = await MessageMedia.fromUrl('https://res.cloudinary.com/dc56gnk0b/image/upload/v1686560829/MCA_NEW_2nd_semester_assignment_January_2023_c0lkly.pdf', { unsafeMime: true });
-            //location
-            message = 'https://www.google.com/maps/place/AGARSON+SAFETY+SHOE/@28.6909549,76.95733,15z/data=!4m6!3m5!1s0x390d09e89161e923:0x7bf949116c2efe71!8m2!3d28.6909549!4d76.95733!16s%2Fg%2F11c56q_2n3?entry=ttu'
-            // message = await client.getContactById(`919****412@c.us`)
-            if (from) {
-                const result = await
-                client.sendMessage(from._serialized, message)
-                console.log("sent message", result)
-            } else {
-                console.log(from, `${from} is not registered`)
-            }
+            const poll = {
+                name: 'new poll',
+                options: [
+                    {
+                        name: 'option 1'
+                    },
+                    {
+                        name: 'option 2'
+                    }
+                ],
+                selectableOptionsCount: 1
+            };
+            await client.sendPollCreation('917404293907@c.us', poll)
+                .then((result) => {
+                    console.log('Result: ', result); //return object success
+                })
+                .catch((erro) => {
+                    console.error('Error when sending: ', erro); //return object error
+                });
         }
+
     }
     catch (err) {
         console.log(err)
