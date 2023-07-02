464e6d1baebe95537eae1f045e52d7efefb74874
nishu kumar <kumarnishu437@gmail.com>
2023-07-02 19:58:34 +0530

    updated

--- controllers/bot.controller.ts
+++ controllers/bot.controller.ts
@@ -24,7 +24,7 @@ export const SetUpWhatsappProfile = async (req: Request, res: Response, next: Ne
 }
 
 export const CreateFlow = async (req: Request, res: Response, next: NextFunction) => {
-    const { flow_name, nodes } = req.body as TFlowBody
+    const { flow_name, nodes, edges } = req.body as TFlowBody
     let startNode = nodes.find(node => node.id === "start")
     let trigger_keywords = ""
     if (startNode && startNode.data) {
@@ -33,6 +33,7 @@ export const CreateFlow = async (req: Request, res: Response, next: NextFunction
     let new_flow = await new Flow({
         flow_name: flow_name,
         nodes: nodes,
+        edges: edges,
         trigger_keywords: trigger_keywords,
         created_at: new Date(),
         updated_at: new Date(),
@@ -44,7 +45,7 @@ export const CreateFlow = async (req: Request, res: Response, next: NextFunction
 
 export const UpdateFlow = async (req: Request, res: Response, next: NextFunction) => {
     const id = req.params.id
-    const { flow_name, nodes } = req.body as TFlowBody
+    const { flow_name, nodes, edges: edges, } = req.body as TFlowBody
     if (!await Flow.findById(id))
         return res.status(404).json({ message: "flow not exists" })
     let startNode = nodes.find(node => node.id === "start")
@@ -55,6 +56,7 @@ export const UpdateFlow = async (req: Request, res: Response, next: NextFunction
     await Flow.findByIdAndUpdate(id, {
         flow_name: flow_name,
         nodes: nodes,
+        edges: edges,
         trigger_keywords: trigger_keywords,
         updated_at: new Date(),
         updated_by: req.user
